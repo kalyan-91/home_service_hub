2d28e508e83dafd616936c20973e5a4d0aa38852
Mohammed11996 <mohammadasif76232@gmail.com>
2026-09-10 18:00:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify
 from config import Config
+from database.connection import init_pool
 
+from routes.auth import auth_bp
+from routes.customer import customer_bp
+from routes.technician import technician_bp
+from routes.services import services_bp
 from routes.admin import admin_bp
 from routes.booking import booking_bp
 from routes.move import move_bp
@@ -10,6 +15,12 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    init_pool()
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(customer_bp)
+    app.register_blueprint(technician_bp)
+    app.register_blueprint(services_bp)
     app.register_blueprint(admin_bp)
     app.register_blueprint(booking_bp)
     app.register_blueprint(move_bp)
