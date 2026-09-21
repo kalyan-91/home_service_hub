8935ceab5f2d81754de3edf21e0be97c8fc5afff
D Pavan Kalyan <163387400+kalyan-91@users.noreply.github.com>
2026-09-21 17:04:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,9 +45,6 @@ def create_app():
     def review_page():
         return render_template("reviews/submit.html")
 
-    @app.route("/payments/summary")
-    def review_page():
-        return render_template("payments/summary.html")
 
 
     # ---------------------------------------------------------------
